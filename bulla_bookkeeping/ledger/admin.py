ccb337a496e5e087daa558f85494efa67db24c2d
George Nagel <gsnagel@gmail.com>
2024-12-23 08:38:11 -0500

    Remove more pycache files. Register models in admin. Ignore sqlitedb in git

--- bulla_bookkeeping/ledger/admin.py
+++ bulla_bookkeeping/ledger/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from ledger.models.account import Account
+from ledger.models.transaction import Transaction
+from ledger.models.transaction_leg import TransactionLeg
+from ledger.models.statement import Statement
+
+admin.site.register(Account)
+admin.site.register(Transaction)
+admin.site.register(TransactionLeg)
+admin.site.register(Statement)
