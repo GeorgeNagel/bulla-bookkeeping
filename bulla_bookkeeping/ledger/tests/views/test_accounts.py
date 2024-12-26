ae58378ae5d0802b9ee925b1ecb971a10644747c
George Nagel <gsnagel@gmail.com>
2024-12-26 09:04:09 -0500

    Add test for account creation view

--- bulla_bookkeeping/ledger/tests/views/test_accounts.py
+++ bulla_bookkeeping/ledger/tests/views/test_accounts.py
@@ -1,10 +1,31 @@
 from django.test import TestCase
 from django.urls import reverse
+from django_bulla.models.normals import Normals
+
+from ledger.models.account import Account
 
 
-class TestAccountsView(TestCase):
+class TestListAccountsView(TestCase):
     def test_success(self):
         url = reverse("accounts")
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
+
+
+class TestCreateAccountView(TestCase):
+    def test_success(self):
+        url = reverse("create_account")
+        parent_account = Account.objects.first()
+        data = {"name": "Foo", "normal": "DEBIT", "parent": parent_account.id}
+
+        response = self.client.post(url, data=data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.headers.get("Location"), reverse("accounts"))
+        self.assertEqual(
+            Account.objects.filter(
+                name="Foo", normal=Normals.DEBIT, parent=parent_account
+            ).count(),
+            1,
+        )
