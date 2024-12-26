5904fc3de5c21d94464ef6fb12bf2ae628f9afd6
George Nagel <gsnagel@gmail.com>
2024-12-26 10:17:06 -0500

    Update create account widget for debit credit dropdown

--- bulla_bookkeeping/ledger/forms/account/create_account_form.py
+++ bulla_bookkeeping/ledger/forms/account/create_account_form.py
@@ -1,11 +1,12 @@
 from django import forms
 
+from django_bulla.models.normals import Normals
 from ledger.models.account import Account
-from ledger.forms.fields.normals import NormalField
+from ledger.forms.fields.normals import NormalField, NORMAL_FIELD_CHOICES
 
 
 class CreateAccountForm(forms.ModelForm):
-    normal = NormalField()
+    normal = NormalField(choices=NORMAL_FIELD_CHOICES)
 
     class Meta:
         model = Account
--- bulla_bookkeeping/ledger/forms/fields/normals.py
+++ bulla_bookkeeping/ledger/forms/fields/normals.py
@@ -2,8 +2,11 @@ from django.core.exceptions import ValidationError
 from django.forms import fields
 from django_bulla.models.normals import Normals
 
+NORMAL_FIELD_CHOICES = {"DEBIT": "Debit", "CREDIT": "Credit"}
 
-class NormalField(fields.Field):
+
+class NormalField(fields.ChoiceField):
+
     def clean(self, value):
         if value == "DEBIT":
             return Normals.DEBIT
