b48c916946d1ba78d924463729791e4af9522baa
George Nagel <gsnagel@gmail.com>
2024-12-26 08:27:26 -0500

    Add accounts page

--- bulla_bookkeeping/ledger/urls.py
+++ bulla_bookkeeping/ledger/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from ledger.views.dashboard import DashboardView
+from ledger.views.accounts import AccountsView
 
 urlpatterns = [
+    path("accounts", AccountsView.as_view(), name="accounts"),
     path("", DashboardView.as_view(), name="dashboard"),
 ]
